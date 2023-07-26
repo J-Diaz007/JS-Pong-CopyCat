7c9232c83ae47d583486756a4f81bae8eb74bf2c
JDiaz <jdiaz_007@outlook.com>
2023-07-25 22:14:00 -0700

    demo commit

---
+++ apps.js
@@ -0,0 +1,104 @@
+const canvas = document.getElementById('pong');
+const ctx = canvas.getContext('2d');
+
+// PADDLE FUNCTION
+function drawRect(x, y, w, h, color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
+// CIRCLE FUNCTION
+function drawArc(x, y, r, color){
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.closePath();
+    ctx.fill();
+}
+
+// TEXT FUNCTION
+function drawText(text, x, y, color){
+    ctx.fillStyle = 'white';
+    ctx.font = '75px fantasy';
+    ctx.fillText(text, x, y);
+}
+
+// HOW TO MOVE A RECTANGLE TO THE RIGHT
+// let rectX = 0;
+// function render(){
+//     drawRect(0, 0, 600, 400, 'black');
+//     drawRect(rectX, 100, 100, 100, 'white');
+//     rectX = rextX + 100;
+// }
+
+// setInterval(render, 1000);
+
+// =======MIGHT NEED TO SWITCH TOP AND BOTTOM=====================================================================
+
+// DRAW USER PADDLE
+const user = {
+    x: 0,
+    y: canvas.height/2 - 50,
+    width: 10,
+    height: 100,
+    color: 'BLUE',
+    score: 0,
+}
+ 
+
+// DRAW COMPUTER PADDLE
+const comp = {
+    x: canvas.width - 10,
+    y: canvas.height/2 - 50,
+    width: 10,
+    height: 100,
+    color: 'RED',
+    score: 0,
+}
+
+// DRAW NET
+const net = {
+    X: canvas.width/2 -2/2,
+    y: 0,
+    width: 2,
+    height: 10,
+    color: 'wHITE',
+}
+
+function drawNet(){
+    for(let i = 0; i <= canvas.height; i+=15){
+        drawRect(net.x, net.y + i, net.width, net.height, net.color);
+    }
+}
+
+// DRAW & MOVE THE BALL
+const ball = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    radius: 10,
+    speed: 5,
+    velocityX: 5,
+    velocityY: 5,
+    color: 'WHITE',
+}
+
+// RENDER THE GAME
+function render(){
+    drawRect(0, 0, canvas.width, canvas.height, 'black');
+    drawText(user.score,canvas.width/4,canvas.height/5, 'white');
+    drawText(comp.score,3*canvas.width/4,canvas.height/5, 'white');
+    drawNet();
+    drawRect(user.x, user.y, user.width, user.height, user.color);
+    drawRect(comp.x, comp.y, comp.width, comp.height, comp.color);
+    drawArc(ball.x, ball.y, ball.radius, ball.color);
+}
+
+function game(){
+    render();
+}
+
+const framePerSecond = 50;
+setInterval(game, 1000/framePerSecond);
+
+
+// COLLISION DETECTION
