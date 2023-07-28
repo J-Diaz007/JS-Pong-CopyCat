4fa77cdd08dc156d2214b81a6f2d990bb36328a7
JDiaz <jdiaz_007@outlook.com>
2023-07-27 19:31:47 -0700

    moved render function to the bottom

--- apps.js
+++ apps.js
@@ -7,6 +7,8 @@ function drawRect(x, y, w, h, color){
     ctx.fillRect(x, y, w, h);
 }
 
+drawRect(0, 0, canvas.width, canvas.height, "black");
+
 // CIRCLE FUNCTION
 function drawArc(x, y, r, color){
     ctx.beginPath();
@@ -15,6 +17,8 @@ function drawArc(x, y, r, color){
     ctx.closePath();
     ctx.fill();
 }
+
+drawArc()
 
 // TEXT FUNCTION
 function drawText(text, x, y, color){
@@ -30,10 +34,8 @@ function drawText(text, x, y, color){
 //     drawRect(rectX, 100, 100, 100, 'white');
 //     rectX = rextX + 100;
 // }
-
 // setInterval(render, 1000);
 
-// =======MIGHT NEED TO SWITCH TOP AND BOTTOM=====================================================================
 
 // DRAW USER PADDLE
 const user = {
@@ -82,6 +84,14 @@ const ball = {
     color: 'WHITE',
 }
 
+// RESET BALL FUNCTION
+function resetBall(){
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+    ball.speed = 5;
+    ball.velocityX = -ball.velocityX;
+}
+
 // MOVE THE BALL
 function update(){
     ball.x +=velocity; X+
@@ -89,14 +99,61 @@ function update(){
     if(ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0){
         velocity = - velocityY;
     }
-// BALL COLLISION
-    if(collision(ball, player)){
+    // BALL COLLISION
+    let user = (ball.x < canvas.width/2) user: comp;
+
+    if(collision(ball, user)){
+        // WHERE THE BALL HITS THE PADDLE
+        let collidePoint = (ball.y - (user.y + user.height/2));
+        collidePoint = collidePoint/(user.height/2);
+        let angleRad = (Math.PI/4) * collidePoint;
+
+        // DIRECTION OF BALL WHEN IT HITS A PADDLE
+        let direction = (ball.x < canvas.width/2) ? 1 : -1;
+
+        ball.velocityX =  ball.speed * Math.cos(angleRad);
+        ball.velocityY =  ball.speed * Math.sin(angleRad);
+
+        // EVERY TIME THE BALL IS HIT THE SPEED INCREASES
+        ball.speed += 0.1;
         
+    }
+    
+    // UPDATES THE SCORE & RESETS THE BALL
+    if(ball.x - ball.radius < 0){
+        comp.score++;
+        resetBall();
+    }else if(ball.x + ball.radius > canvas.width){
+        user.score++;
     }
 }
 
+// COLLISION DETECTION FUNCTION
+// b = ball, u = user
+function collison (b, p){
+    p.top = p.y;
+    p.bottom = p.y + p.height;
+    p.left = p.x;
+    p.right = p.x + p.width;
 
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x + b.radius;
 
+    return b.right > p.left && b.btop < p.bottom && b.left < p.right && b.bottom > p.top;
+}
+
+// CONTROL THE USER'S PADDLE
+canvas.addEventListener('mousemove', movePaddle);
+function movePaddle(evt){
+    let rect = canvas.getBoundingClientRect();
+    user.y = evt.clientY - rect.top - user.height/2;
+}
+
+// AI TO CONTROL THE COMP PADDLE
+let computerLevel = 0.1;
+comp.y += (ball.y - (comp.y + comp.height/2)) * computerLevel;
 
 // RENDER THE GAME
 function render(){
@@ -117,20 +174,3 @@ const framePerSecond = 50;
 setInterval(game, 1000/framePerSecond);
 
 
-// COLLISION DETECTION FUNCTION
-// b = ball, p = player
-function collison (b, p){
-    p.top = p.y;
-    p.bottom = p.y + p.height;
-    p.left = p.x;
-    p.right = p.x + p.width;
-
-    b.top = b.y - b.radius;
-    b.bottom = b.y + b.radius;
-    b.left = b.x - b.radius;
-    b.right = b.x + b.radius;
-
-    return b.right > p.left && b.btop < p.bottom && b.left < p.right && b.bottom > p.top;
-}
-
-// LEFT OFF ON 22 MIN & 30 SECONDS OF VIDEO
